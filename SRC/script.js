101947f98359259a11ad1a615bce775ec79faa68
Сергей Андреевич <65599282+Vigitoreanec@users.noreply.github.com>
2024-10-16 13:01:20 +0200

    Update script.js
    man ->

--- SRC/script.js
+++ SRC/script.js
@@ -3,8 +3,8 @@ function
   sayHello(event) {
   const
     hello = `Hello, `,
-    user = `User!!! `,
-    from = 'From ';
+    user = `User man`,
+    from = 'from ';
   alert(
     hello + user + from + event.target.tagName
   )
